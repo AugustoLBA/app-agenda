725d7f1d6e4a87579f1d0c0f069e0a1956cb6efa
Augusto LBA <j.augusto.lba@gmail.com>
2024-04-24 13:35:45 -0300

    Adicionando o metodo saveContato na classe ContatoController

--- src/main/java/com/appagenda/controller/ContatoController.java
+++ src/main/java/com/appagenda/controller/ContatoController.java
@@ -1,6 +1,9 @@
 package com.appagenda.controller;
 
+import com.appagenda.dto.ContatoDTO;
+import com.appagenda.entity.Contato;
 import com.appagenda.service.ContatoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -9,4 +12,9 @@ public class ContatoController {
 
     @Autowired
     private ContatoService service;
+
+    public void saveContato(@Valid ContatoDTO contatoDTO){
+        Contato contato = service.toContato(contatoDTO);
+        service.salvarContato(contato);
+    }
 }
