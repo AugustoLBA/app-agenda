412ad8ed9967eaaf019f0409c69a176521fe4ea6
Augusto LBA <j.augusto.lba@gmail.com>
2024-04-24 18:54:17 -0300

    Adicionando botoes a classe JanelaInicial

--- src/main/java/com/appagenda/ui/janelas/JanelaInicial.java
+++ src/main/java/com/appagenda/ui/janelas/JanelaInicial.java
@@ -1,10 +1,13 @@
 package com.appagenda.ui.janelas;
 
+import com.appagenda.ui.utils.JButtonPadrao;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import java.awt.*;
+
 @Getter
 @Setter
 @Component("JanelaInicial")
@@ -14,8 +17,6 @@ public class JanelaInicial extends JanelaPadrao {
 
     private JButton visualizarContatos;
 
-    private JButton editarContato;
-
     private JButton sair;
     @Override
     public void addImg() {
@@ -24,6 +25,15 @@ public class JanelaInicial extends JanelaPadrao {
 
     @Override
     public void addJButton() {
+
+        novoContato = JButtonPadrao.criarJbutton("Novo Contato",275,250,150,30, Color.YELLOW);
+        this.add(novoContato);
+
+        visualizarContatos = JButtonPadrao.criarJbutton("Visualizar Contatos",275,300,150,30,Color.YELLOW);
+        this.add(visualizarContatos);
+
+        sair = JButtonPadrao.criarJbutton("Sair",275,350,150,30,Color.YELLOW);
+        this.add(sair);
 
     }
 
