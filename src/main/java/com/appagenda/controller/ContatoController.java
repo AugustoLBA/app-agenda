b93cd229b71efb48d6260194ed6e0444213195de
Augusto LBA <j.augusto.lba@gmail.com>
2024-04-24 13:38:13 -0300

    Adiconando o metodo findAll na classe ContatoController

--- src/main/java/com/appagenda/controller/ContatoController.java
+++ src/main/java/com/appagenda/controller/ContatoController.java
@@ -1,11 +1,14 @@
 package com.appagenda.controller;
 
 import com.appagenda.dto.ContatoDTO;
+import com.appagenda.dto.ContatoResponseDTO;
 import com.appagenda.entity.Contato;
 import com.appagenda.service.ContatoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class ContatoController {
@@ -16,5 +19,9 @@ public class ContatoController {
     public void saveContato(@Valid ContatoDTO contatoDTO){
         Contato contato = service.toContato(contatoDTO);
         service.salvarContato(contato);
+    }
+
+    public List<ContatoResponseDTO> findAll(){
+        return service.toListDto(service.listarContatos());
     }
 }
