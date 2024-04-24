b857132198573e43d27e590aeb80abb2dcdd1e9f
Augusto LBA <j.augusto.lba@gmail.com>
2024-04-24 14:32:07 -0300

    Adicionando a excecao EntityNotFoundException

--- src/main/java/com/appagenda/AppAgendaApplication.java
+++ src/main/java/com/appagenda/AppAgendaApplication.java
@@ -1,13 +1,24 @@
 package com.appagenda;
 
+import com.appagenda.controller.ContatoController;
+import com.appagenda.dto.ContatoResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppAgendaApplication {
+public class AppAgendaApplication implements CommandLineRunner {
+	@Autowired
+	private ContatoController controller;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppAgendaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		ContatoResponseDTO dto = controller.findById(8L);
+		System.out.println(dto.toString());
+	}
 }
---
+++ src/main/java/com/appagenda/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.appagenda.exception;
+
+public class EntityNotFoundException extends RuntimeException{
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+    
+}
