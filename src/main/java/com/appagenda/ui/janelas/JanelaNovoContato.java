2b5931f46280e34af0c18fd75ae2f52357f368fe
Augusto LBA <j.augusto.lba@gmail.com>
2024-04-25 08:29:54 -0300

    Adicionando a classe JanelaNovoContato

---
+++ src/main/java/com/appagenda/ui/janelas/JanelaNovoContato.java
@@ -0,0 +1,23 @@
+package com.appagenda.ui.janelas;
+
+public class JanelaNovoContato extends JanelaPadrao{
+    @Override
+    public void addImg() {
+
+    }
+
+    @Override
+    public void addJButton() {
+
+    }
+
+    @Override
+    public void addJTextField() {
+
+    }
+
+    @Override
+    public void addJLbel() {
+
+    }
+}
